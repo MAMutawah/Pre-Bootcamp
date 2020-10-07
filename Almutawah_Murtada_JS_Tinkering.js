2e9605335061a91c1d4cf375b211e67fae187c68
Murtada Almutawah <mamutawah@gmail.com>
2020-10-07 23:09:54 +0300

    More assignments

---
+++ Almutawah_Murtada_JS_Tinkering.js
@@ -0,0 +1,11 @@
+var childHeight = 168;
+
+function displayIfChildIsAbleToRideTheRollerCoaster(childHeight) {
+    if (childHeight > 52) {
+        console.log("Get on that ride, kiddo!");
+    } else {
+        console.log("Sorry kiddo. Maybe next year.");
+    }
+}
+
+displayIfChildIsAbleToRideTheRollerCoaster(childHeight);
